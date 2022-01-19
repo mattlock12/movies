678eb6be2e74860e83e3395a84967f40675ab74b
John Hooper <nisse038@gmail.com>
2022-01-19 00:40:10 -0600

    get nextjs up and running

---
+++ pages/movies/index.tsx
@@ -0,0 +1,37 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { promises as fs } from 'fs';
+import path from 'path';
+import Link from 'next/link';
+
+export interface MovieInterface {
+  id: number,
+  movie: string,
+  dirname: string
+}
+
+export const getStaticProps: GetStaticProps = async (ctx ) => {
+
+  const moviesDirectory = path.join(process.cwd(), 'data/movies')
+  const jsonFile = await fs.readFile(path.join(moviesDirectory, 'movies.json'), 'utf8')
+  const movies = JSON.parse(jsonFile)
+
+  return {
+    props: {
+      movies
+    }
+  }
+}
+
+const MovieHomePage = ({movies}: {movies:MovieInterface[]}) => {
+  return <ul>
+    {
+      movies.map(movie => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.movie}</Link>
+        </li>
+      ))
+    }
+  </ul>
+
+}
+export default MovieHomePage
