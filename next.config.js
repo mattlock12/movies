96a8e83ff6e75eb6d45eff1fae983b1bd5a6db18
John Hooper <nisse038@gmail.com>
2022-04-05 22:23:51 -0500

    i forget why i added this stuff but it looks like its working well enough

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const nextConfig = {
+  images: {
+    disableStaticImages: true,
+  },
   reactStrictMode: true,
-}
+};
+
+const config = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        // optimisation disabled by default, to enable check https://github.com/cyrilwanner/next-optimized-images
+        optimizeImages: false,
+      },
+    ],
+  ],
+  nextConfig
+);
+
+module.exports = config
